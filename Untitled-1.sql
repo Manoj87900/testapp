2f55c572db6c63edca7fcc38144fbc3729c01af5
manoj87900 <manojsannapaneni@gmail.com>
2023-01-07 18:35:33 -0600

    change to new file

--- Untitled-1.sql
+++ Untitled-1.sql
@@ -7,12 +7,6 @@ CREATE TABLE [dbo].[tbldepartment](
 	[Dname] [nvarchar](50) NOT NULL,
 	[Location] [nvarchar](50) NOT NULL,
 	[Departmenthead] [nvarchar](50) NOT NULL
-) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[tbldepartment] ADD  CONSTRAINT [PK_tbldepartment] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
+) 
 
 
