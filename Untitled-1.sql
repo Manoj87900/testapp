b959fc6730253aceb869b0e2080a612265b7b1c1
manoj87900 <manojsannapaneni@gmail.com>
2023-01-07 18:31:43 -0600

    second test commit

---
+++ Untitled-1.sql
@@ -0,0 +1,18 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[tbldepartment](
+	[Id] [int] NOT NULL,
+	[Dname] [nvarchar](50) NOT NULL,
+	[Location] [nvarchar](50) NOT NULL,
+	[Departmenthead] [nvarchar](50) NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[tbldepartment] ADD  CONSTRAINT [PK_tbldepartment] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+
+
